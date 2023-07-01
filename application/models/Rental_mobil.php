3ed4d63af2fb0af21575e491a61a7ebe12abce45
prillyalda <128234634+prillyalda@users.noreply.github.com>
2023-07-01 22:03:41 +0700

    detail dan delete data mobil

--- application/models/Rental_mobil.php
+++ application/models/Rental_mobil.php
@@ -15,6 +15,20 @@
 
 		public function update_data($table, $data, $where){
 			$this->db->update($table, $data, $where);
-		}		
+		}
+		
+		public function delete_data($where, $table){
+			$this->db->where($where);
+			$this->db->delete($table);
+		}
+		
+		public function ambil_id_mobil($id){
+			$hasil = $this->db->where('id_mobil',$id)->get('mobil');
+			if($hasil->num_rows() > 0){
+				return $hasil->result();
+			}else{
+				return false;
+			}
+		}
 	}
-?>
+ ?>
